cc8eafa1ca8ba6a004d31324537b2f5f8d57e748
Charles Butler <cdbutle@outlook.com>
2020-03-13 16:55:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
 
-
+def my_collect(array)
+  i = 0
+  collection = []
+  while i< array.length
+  #itterate over a collection using a while loop
+end
+collection
+end
+  
+  
+  #execute code in a block you call it with for each element in the collection
+  #returns modified collection
+my_collect
